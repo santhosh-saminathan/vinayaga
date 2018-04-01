a24d17e4eecd8b32ba2ba5566eac34d8d7845e87
santhosh <santhosh.developer@yahoo.com>
2018-04-01 12:34:56 +0530

    changed api url

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -9,20 +9,18 @@ export class BillService {
     constructor(private http: HttpClient) { }
 
     storeBill(data) {
-        //return this.http.post("https://nodeexpress-app.herokuapp.com/store/bill", data);
-        return this.http.post("http://localhost:5000/store/bill", data);
+        return this.http.post("https://nodeexpress-app.herokuapp.com/store/bill", data);
+        //return this.http.post("http://localhost:5000/store/bill", data);
     }
 
     getDetails(data) {
-        // return this.http.post("https://nodeexpress-app.herokuapp.com/bill", data);
-
-        return this.http.post("http://localhost:5000/bill", data);
-
+        return this.http.post("https://nodeexpress-app.herokuapp.com/bill", data);
+        //return this.http.post("http://localhost:5000/bill", data);
     }
 
     deleteBill(data){
-        return this.http.post("http://localhost:5000/invoice/delete", data);
-        
+        return this.http.post("https://nodeexpress-app.herokuapp.com/invoice/delete", data);
+        // return this.http.post("http://localhost:5000/invoice/delete", data);
     }
 
 
