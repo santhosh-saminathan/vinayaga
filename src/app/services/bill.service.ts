8efd555b95111a9436b3513931c7795dee40c40e
santhosh-saminathan <36120220+santhosh-saminathan@users.noreply.github.com>
2018-07-13 10:14:13 +0530

    Update bill.service.ts

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -8,34 +8,34 @@ export class BillService {
     constructor(private http: HttpClient) { }
 
     storeBill(data) {
-        // return this.http.post("https://nodeexpress-app.herokuapp.com/store/bill", data);
-        return this.http.post("http://localhost:5000/store/bill", data);
+        return this.http.post("https://nodeexpress-app.herokuapp.com/store/bill", data);
+//         return this.http.post("http://localhost:5000/store/bill", data);
     }
 
     getDetails(data) {
-        // return this.http.post("https://nodeexpress-app.herokuapp.com/bill", data);
-        return this.http.post("http://localhost:5000/bill", data);
+        return this.http.post("https://nodeexpress-app.herokuapp.com/bill", data);
+//         return this.http.post("http://localhost:5000/bill", data);
     }
 
     deleteBill(data) {
-        // return this.http.post("https://nodeexpress-app.herokuapp.com/invoice/delete", data);
-        return this.http.post("http://localhost:5000/invoice/delete", data);
+        return this.http.post("https://nodeexpress-app.herokuapp.com/invoice/delete", data);
+//         return this.http.post("http://localhost:5000/invoice/delete", data);
     }
 
     getSingleBill(data) {
-        // return this.http.post("https://nodeexpress-app.herokuapp.com/single/bill", data);
+        return this.http.post("https://nodeexpress-app.herokuapp.com/single/bill", data);
 
-        return this.http.post("http://localhost:5000/single/bill", data);
+//         return this.http.post("http://localhost:5000/single/bill", data);
 
     }
     updateBill(data) {
-        // return this.http.post("https://nodeexpress-app.herokuapp.com/update/bill", data);
-        return this.http.post("http://localhost:5000/update/bill", data);
+        return this.http.post("https://nodeexpress-app.herokuapp.com/update/bill", data);
+//         return this.http.post("http://localhost:5000/update/bill", data);
     }
 
     billByCompany(data) {
-        // return this.http.post("https://nodeexpress-app.herokuapp.com/update/bill", data);
-        return this.http.post("http://localhost:5000/bill/company", data);
+        return this.http.post("https://nodeexpress-app.herokuapp.com/update/bill", data);
+//         return this.http.post("http://localhost:5000/bill/company", data);
     }
 
 
